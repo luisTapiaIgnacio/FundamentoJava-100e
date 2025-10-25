84cfadd426753f75d83838ca0a435c5c86b2dc8f
zoe-alice <publicidad-aym@hotmail.com>
2025-10-25 06:42:34 -0500

    5 advanced exercises-1

--- FundamentoJava-100e/ESTRUCTURA_SECUENCIAL/Question02.java
+++ FundamentoJava-100e/ESTRUCTURA_SECUENCIAL/Question02.java
@@ -4,7 +4,7 @@ public class Question02 {
     public static void main(String[] args) {
         int dividendo=9;
         int divisor=2;
-        int resto=9%2;
+        int resto=dividendo%divisor;
         int cociente=9/2;
 
         System.out.println("el cociente es: "+cociente);
