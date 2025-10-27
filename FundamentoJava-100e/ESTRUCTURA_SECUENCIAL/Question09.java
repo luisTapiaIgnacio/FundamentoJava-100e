9c26b9ba4ecea43857447b485c629ed28da2a3fc
zoe-alice <publicidad-aym@hotmail.com>
2025-10-27 07:48:06 -0500

    dom-avance ejer

--- FundamentoJava-100e/ESTRUCTURA_SECUENCIAL/Question09.java
+++ FundamentoJava-100e/ESTRUCTURA_SECUENCIAL/Question09.java
@@ -1,4 +1,16 @@
 package ESTRUCTURA_SECUENCIAL;
+
+import java.util.Scanner;
+
 // Crear un programa para encontrar el área de un círculo, use la fórmula:
 public class Question09 {
+    public static void main(String[] args) {
+        final float PI=3.14f;
+        float area, radio;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese el radio:" );
+        radio=sc.nextFloat();
+        area=(float)(PI*(Math.pow((double) radio, 2.0)));
+        System.out.println("el area es: "+area);
+    }
 }
